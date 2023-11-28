3bd1c271dcb77770c5e378b90b8df56350801a01
lekai63 <lekai63@gmail.com>
2023-11-28 17:26:06 +0000

    update

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -1,5 +1,5 @@
 // components/ChatBox/ChatBox.tsx
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import MessageList from './MessageList';
 import InputBox from './InputBox';
 import { ChatContext } from '../../contexts/ChatContext';
@@ -12,7 +12,13 @@ const generateUniqueId = () => messageIdCounter++;
 const ChatBox = () => {
   const { messages, setMessages } = useContext(ChatContext);
   const [newMessage, setNewMessage] = useState('');
+  const [aiThinking, setAiThinking] = useState(false);
+  const aiThinkingTimer = useRef<NodeJS.Timeout | null>(null);
 
+
+  const aiThinkingTimeLimit = 30; // 30 seconds countdown
+
+  // 建立与服务器的SSE连接，监听新消息
   useEffect(() => {
     const userId = localStorage.getItem('userId');
     if (!userId) {
@@ -23,6 +29,7 @@ const ChatBox = () => {
     console.log(`Establishing SSE connection with userId: ${userId}`);
     const eventSource = new EventSource(`/api/events?userId=${userId}`);
 
+
     eventSource.addEventListener('customMessage', (event) => {
       console.log("Received SSE data:", event.data);
       try {
@@ -30,16 +37,15 @@ const ChatBox = () => {
         console.log("Parsed SSE data:", data);
 
         if (aiThinkingMessageId !== 0) {
+          setAiThinking(false); // Stop displaying AI thinking message
+          if (aiThinkingTimer.current !== null) {
+            clearTimeout(aiThinkingTimer.current);
+          } // Clear the countdown timer
           setMessages(prevMessages => prevMessages.map(msg =>
             msg.id === aiThinkingMessageId ? { ...msg, text: data.aiMessage, isUser: false, timestamp: new Date() } : msg
           ));
           aiThinkingMessageId = 0; // Reset the aiThinkingMessageId
-        } else {
-          setMessages(prevMessages => [
-            ...prevMessages,
-            { id: generateUniqueId(), text: data.aiMessage, isUser: false, timestamp: new Date() }
-          ]);
-        }
+        } 
       } catch (error) {
         console.error('Error parsing SSE data:', error);
       }
@@ -55,48 +61,65 @@ const ChatBox = () => {
     };
   }, [setMessages]);
 
-  const handleSendMessage = async () => {
+   // 处理发送消息的逻辑
+   const handleSendMessage = async () => {
+    if (aiThinkingMessageId !== 0) return; // Prevent sending if AI is thinking
+
     const userId = localStorage.getItem('userId');
     if (newMessage.trim()) {
+      // 先在界面上显示用户消息
       setMessages(prevMessages => [
         ...prevMessages,
         { id: generateUniqueId(), text: newMessage, isUser: true, timestamp: new Date() }
       ]);
   
+      // 清空输入框
+      setNewMessage('');
+
+      // 模拟AI正在思考的消息
       aiThinkingMessageId = generateUniqueId();
+
       setMessages(prevMessages => [
         ...prevMessages,
         { id: aiThinkingMessageId, text: "AI is thinking...", isUser: false, timestamp: new Date() }
       ]);
   
       try {
+        // 向服务器发送消息
         await axios.post('/api/send-message', { message: newMessage, userId: userId });
-        setNewMessage(''); // 清空输入框
       } catch (error) {
         console.error('Error sending message:', error);
       }
     }
+
+    setAiThinking(true); // Start displaying AI thinking message
+    aiThinkingTimer.current = setTimeout(() => {
+      setAiThinking(false); // Stop displaying AI thinking message after 30 seconds
+    }, aiThinkingTimeLimit * 1000);
   };
 
+  // 处理输入框变化
   const handleInput = (event) => {
     setNewMessage(event.target.value);
   };
 
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      handleSendMessage();
-    }
-  };
+  // 处理键盘按键事件
+const handleKeyPress = (event) => {
+  if (event.key === 'Enter' && aiThinkingMessageId === 0) {
+    handleSendMessage();
+  }
+};
 
-  return (
-    <div className="chat-box flex flex-col h-full">
-      <MessageList messages={messages} />
+return (
+  <div className="chat-box flex flex-col h-full overflow-auto"> {/* Added overflow-auto */}
+    <MessageList messages={messages} />
       <InputBox
         value={newMessage}
         onChange={handleInput}
         onKeyPress={handleKeyPress}
         onSend={handleSendMessage}
       />
+        {aiThinking && <div className="ai-thinking-message">AI is thinking... {aiThinkingTimeLimit} seconds</div>}
     </div>
   );
 };
--- src/components/ChatBox/Message.tsx
+++ src/components/ChatBox/Message.tsx
@@ -6,7 +6,7 @@ const Message = ({ isUser, text }) => {
 <div className={`chat-message ${isUser ? 'user-message' : 'ai-message'}`}>
   {isUser ? (
     <>
-      <div className="icon-container ml-2"><User theme="outline" size="24" fill="#ffffff"/></div> {/* User icon on the left */}
+      <div className="icon-container ml-2"><User theme="outline" size="24" fill="#2d3748"/></div> {/* User icon on the left */}
       <div className="message-content">{text}</div>
     </>
   ) : (
