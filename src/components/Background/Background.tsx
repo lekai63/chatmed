3396f4649eac716cd58d3bc6f14b2963889406e7
lekai63 <lekai63@gmail.com>
2023-11-28 13:56:53 +0000

    fix background compile

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const Background = () => {
-  const backgroundStyle = {
-    width: '100%',               // 宽度为100%
-    height: '100vh',             // 高度为视窗的100%
-    backgroundImage: 'url(/images/background.png)', // 使用图片路径
-    backgroundSize: 'cover',     // 覆盖整个区域
-    backgroundPosition: 'center', // 居中背景图片
-    backgroundRepeat: 'no-repeat', // 不重复背景图片
-    position: 'fixed',           // 固定定位
-    top: 0,                      // 从顶部开始
-    left: 0,                     // 从左侧开始
-    zIndex: -1                   // 确保背景在内容之下
+  const backgroundStyle: React.CSSProperties = {
+    width: '100%',
+    height: '100vh',
+    backgroundImage: 'url(/images/background.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    position: 'fixed', // 确保使用明确的字符串类型
+    top: 0,
+    left: 0,
+    zIndex: -1
   };
 
   return <div style={backgroundStyle} />;
