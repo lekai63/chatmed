40793663d2797f717c966ddfe506b1e82e71cea5
lekai <lekai63@gmail.com>
2023-12-06 16:10:28 +0800

    update

--- src/utils/corsMiddleware.ts
+++ src/utils/corsMiddleware.ts
@@ -2,10 +2,21 @@
 import Cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 // 初始化CORS中间件，根据需要调整配置
-const cors = Cors({
-  origin: [process.env.CDN_URL,process.env.NEXT_PUBLIC_API_BASE_URL], 
-  methods: ['GET', 'HEAD', 'POST'], // 根据需要调整允许的HTTP方法
-});
+
+const allowedOrigins = [process.env.CDN_URL || 'https://pc.xxhzjk.com',process.env.NEXT_PUBLIC_API_BASE_URL || "https://deeplx.xxhzjk.com"];
+
+const corsOptions: Cors.CorsOptions  = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'HEAD', 'POST'],
+};
+
+const cors = Cors(corsOptions);
 
 // 导出cors对象
 export { cors };
